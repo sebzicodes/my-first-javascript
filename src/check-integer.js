9edb46b166757f6fc2ddb20c351501546d1c9a97
Gabriel <gabrieldavis1901@gmail.com>
2026-04-16 13:05:05 -0700

    finished check integer test

--- src/check-integer.js
+++ src/check-integer.js
@@ -1,3 +1,5 @@
-module.export = function checkInteger(num) {
-    return Number.isInteger(num);
-};
+module.exports = function checkInteger(num) {
+  return Number.isInteger(num);
+}
+
+ 
--- tests/check-integer.test.js
+++ tests/check-integer.test.js
@@ -2,11 +2,11 @@ const checkInteger = require("../src/check-integer");
 describe("checkInteger", () => {
     test("returns true for whole numbers", () => {
         expect(checkInteger(500)).toBe(true);
-    })
+    });
     test("returns false for decimals", () => {
         expect(checkInteger(5.5)).toBe(false);
-    })
+    });
     test("returns false for strings that look like numbers", () => {
         expect(checkInteger("10")).toBe(false);
-    })
-})
+    });
+});
