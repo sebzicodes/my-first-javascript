b6e7425187107e7e829ff3cb830ee306523b7736
Gabriel <gabrieldavis1901@gmail.com>
2026-04-25 22:23:09 -0700

    made get-absolute test

---
+++ tests/absolute-value.test.js
@@ -0,0 +1,12 @@
+const getAbsolute = require("../src/getAbsolute");
+describe("getAbsolute", () => {
+    test("converts a negative number to a positive", () => {
+        expect(getAbsolute(-5)).toBe(5);
+    });
+    test("keeps positive number as is", () => {
+        expect(getAbsolute(10)).toBe(10);
+    });
+    test("returns 0 for 0", () => {
+        expect(getAbsolute(0)).toBe(0);
+    })
+});
