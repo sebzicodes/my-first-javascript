939e00ec092fe9ee0693942744a7b86b5c3711f4
Gabriel <gabrieldavis1901@gmail.com>
2026-04-25 21:41:39 -0700

    completed find index by indexing array and comparing to value

--- src/find-index.js
+++ src/find-index.js
@@ -1,1 +1,6 @@
-module.export = function findIndex()
+module.exports = function findIndex(array, value) {
+   for(let i = 0; i < array.length; i++) {
+        if (array[i] === value) {return i;}
+   }
+   return -1;
+};
