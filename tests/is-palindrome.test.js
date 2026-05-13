2de651c2186f0381c8d837b0f3cf7fd1af02959b
Gabriel <gabrieldavis1901@gmail.com>
2026-05-13 12:58:10 -0700

    made is palindrome test

---
+++ tests/is-palindrome.test.js
@@ -0,0 +1,12 @@
+const isPalindrome = require("../src/is-palindrome");
+describe("isPalindrome", () => {
+    test("returns true for valid palindrome", () => {
+        expect(isPalindrome(racecar)).toBe(true);
+    });
+    test("returns false for non-palindrome", () => {
+        expect(isPalindrome(hello)).toBe(false);
+    });
+    test("returns true for an empty string", () => {
+        expect(isPalindrome("")).toBe(true);
+    });
+ });
