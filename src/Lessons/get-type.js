5e81587f816b1b6098a4ea22ae48345278138acd
Gabriel <gabrieldavis1901@gmail.com>
2026-06-15 02:02:19 -0700

    first lesson started - get-type

---
+++ src/Lessons/get-type.js
@@ -0,0 +1,10 @@
+module.exports = function getType(value) {
+    if(value === []) {
+        return console.log(object);
+    }
+    if(value === null) {
+        return console.log(null);
+    }
+    const type = value.typeof;
+    return type;
+};
---
+++ tests/get-type.test.js
@@ -0,0 +1,24 @@
+const getType = require("../src/get-type");
+describe("get-type", () => {
+    test("gets type of an object", () => {
+        expect(getType({})).toBe("object");
+    });
+    test("gets type of a number", () => {
+        expect(getType(42)).toBe("number");
+    });
+    test("gets type of an array", () => {
+        expect(getType([1, 2, 3])).toBe("array");
+    });
+    test("gets type of a boolean", () => {
+        expect(getType(true)).toBe("boolean");
+    });
+    test("gets type of a null", () => {
+        expect(getType(null)).toBe("null");
+    });
+    test("gets type of a undefined", () => {
+        expect(getType(undefined)).toBe("undefined");
+    });
+    test("gets type of a string", () => {
+        expect(getType("hello")).toBe("string");
+    });
+});
